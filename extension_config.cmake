ad1896aa7b10fc71dc2ffd2df5bfb610bcf83f72
Tmonster <tom@ebergen.com>
2026-07-14 15:31:29 +0200

    disable postgres scanner building to enable CI

--- extension_config.cmake
+++ extension_config.cmake
@@ -7,12 +7,13 @@ duckdb_extension_load(aws
 )
 
 # Build the postgres scanner for the redshift action (e.g. redshift attach).
-# Pinned to the ref/patches/submodule duckdb itself uses for this duckdb version, see
-# duckdb/.github/config/extensions/postgres_scanner.cmake
-duckdb_extension_load(postgres_scanner
-    DONT_LINK
-    GIT_URL https://github.com/duckdb/duckdb-postgres
-    GIT_TAG f77b0cb511748fd70fb8a4eb265e2990599d286c
-    SUBMODULES database-connector
-    APPLY_PATCHES
-)
+# Currently Disabled since this will not build on CI. To get CI working you (most likely)
+# need to copy the vcpkg_ports/libpq in the duckdb/duckdb-postgres repo. 
+# For now commented out so unblock development
+# duckdb_extension_load(postgres_scanner
+#    DONT_LINK
+#    GIT_URL https://github.com/duckdb/duckdb-postgres
+#    GIT_TAG f77b0cb511748fd70fb8a4eb265e2990599d286c
+#    SUBMODULES database-connector
+#    APPLY_PATCHES
+#)
